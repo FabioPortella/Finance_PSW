67cc6a1cf491ec15cb40e697ae87c11165cf423f
FabioPortella <fabioportella2009@gmail.com>
2023-07-07 20:01:17 -0300

    definir contas

--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.messages import constants
 from perfil.models import Categoria
+from .models import ContaPagar
 
 def definir_contas(request):
     if request.method == "GET":
@@ -8,3 +11,25 @@ def definir_contas(request):
             'categorias': categorias
         }
         return render(request, 'definir_contas.html', context)
+    else:
+        titulo = request.POST.get('titulo')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        valor = request.POST.get('valor')
+        dia_pagamento = request.POST.get('dia_pagamento')
+
+        conta = ContaPagar(
+            titulo = titulo,
+            categoria_id = categoria,
+            descricao = descricao,
+            valor = valor,
+            dia_pagamento = dia_pagamento,
+        )
+        conta.save()
+        messages.add_message(request, constants.SUCCESS, "Conta cadastrada com sucesso")
+
+        return redirect('/contas/definir_contas')
+
+
+def ver_contas(request):
+    return render(request, 'ver_contas.html')
